b4a77b113b7ef8fa5aa2fbeaabf5dcacd9c61c77
Harshul Jain <harshulj@gmail.com>
2012-10-15 19:34:20 +0530

    Added faculty model and facultyAdmin.

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,11 @@
-from courses.models import Course
+from courses.models import *
 from django.contrib import admin
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ['code', 'name', 'slug']
 
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'slug']
+
+admin.site.register(Faculty, FacultyAdmin)
 admin.site.register(Course, CourseAdmin)
--- courses/models.py
+++ courses/models.py
@@ -4,3 +4,13 @@ class Course(models.Model):
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=60)
+
+class Faculty(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    code = models.CharField(max_length=5)
+    slug = models.SlugField(max_length=50)
+
+    @property
+    def name(self):
+        return self.first_name + ' ' + self.last_name

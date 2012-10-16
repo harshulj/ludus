f0d72d7a24799f7cd07bb365cf338f0c74fcbf40
Harshul Jain <harshulj@gmail.com>
2012-10-16 14:37:26 +0530

    Added classtime to admin.

--- courses/admin.py
+++ courses/admin.py
@@ -7,5 +7,9 @@ class CourseAdmin(admin.ModelAdmin):
 class FacultyAdmin(admin.ModelAdmin):
     list_display = ['code', 'name', 'slug']
 
+class ClassTimeAdmin(admin.ModelAdmin):
+    list_display = ['course', 'day', 'start_time', 'end_time']
+
+admin.site.register(ClassTime, ClassTimeAdmin)
 admin.site.register(Faculty, FacultyAdmin)
 admin.site.register(Course, CourseAdmin)

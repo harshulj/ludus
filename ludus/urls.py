7a01d6c904420b8eb6ad5df3139877db73b86bf6
Harshul Jain <harshulj@gmail.com>
2012-10-16 09:39:06 +0530

    Added home page. And a url to home page.

--- ludus/urls.py
+++ ludus/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from courses.views import *
+from django.views.generic.simple import direct_to_template
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -15,5 +16,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^courses/', courses)
+    url(r'^courses/$', courses),
+    url(r'^$', direct_to_template, {'template': 'index.html'}),
 )

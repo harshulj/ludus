1af7a5b74fc0de0f75ee8254265869db2cf51d57
Harshul Jain <harshulj@gmail.com>
2012-10-16 21:00:34 +0530

    Now classes return in ascending order of timings.

--- courses/views.py
+++ courses/views.py
@@ -11,9 +11,9 @@ def schedule(request, course_id=-1):
     if course_id == -1:
         timings = ClassTime.objects.all().order_by('day', 'start_time')
     else:
-        timings = ClassTime.objects.filter(course=course_id)
+        timings = ClassTime.objects.filter(course=course_id).order_by('day', 'start_time')
     return render_to_response('schedule.html', {'timings' : timings})
 
 def day_schedule(request, day):
-    timings = ClassTime.objects.filter(day=day)
+    timings = ClassTime.objects.filter(day=day).order_by('start_time')
     return render_to_response('schedule.html', {'timings':timings})
